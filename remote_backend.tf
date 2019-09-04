972360b6f3462807391402c0d771f949ac9a2a6a
Ryan Maker <rmaker@costco.com>
2019-09-04 21:43:26 +0000

    skeeeeet

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "ryanmaker-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-azure"
     }
   }
 }
